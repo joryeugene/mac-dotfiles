3ab44609db04e6761e792b14c60c866994582af5
Jory <jory@workhelix.com>
2024-08-28 01:52:20 -0500

    getting nvim changes coming in, subtle cursor

--- init.lua
+++ init.lua
@@ -1,3 +1,5 @@
+-- Init.lua: Comprehensive Neovim configuration
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -6,19 +8,20 @@ if not vim.loop.fs_stat(lazypath) then
     "clone",
     "--filter=blob:none",
     "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
+    "--branch=stable",
     lazypath,
   })
 end
 vim.opt.rtp:prepend(lazypath)
 
--- Set leader key
+-- Leader key
 vim.g.mapleader = " "
 
 -- Plugin configuration
 require("lazy").setup({
-  -- Color scheme
+  -- Theme options
   { "Mofiqul/dracula.nvim" },
+  { "folke/tokyonight.nvim" },
 
   -- Treesitter for better syntax highlighting
   {
@@ -26,10 +29,38 @@ require("lazy").setup({
     build = ":TSUpdate",
     config = function()
       require("nvim-treesitter.configs").setup({
-        ensure_installed = { "lua", "vim", "vimdoc", "python" },
+        ensure_installed = { "lua", "vim", "vimdoc", "python", "javascript", "typescript", "html", "css", "markdown", "yaml", "json", "dockerfile" },
         highlight = { enable = true },
       })
     end,
+  },
+
+  -- LSP
+  {
+    "neovim/nvim-lspconfig",
+    dependencies = {
+      "williamboman/mason.nvim",
+      "williamboman/mason-lspconfig.nvim",
+    },
+  },
+
+  -- Autocompletion
+  {
+    "hrsh7th/nvim-cmp",
+    dependencies = {
+      "hrsh7th/cmp-nvim-lsp",
+      "hrsh7th/cmp-buffer",
+      "hrsh7th/cmp-path",
+      "hrsh7th/cmp-cmdline",
+      "L3MON4D3/LuaSnip",
+      "saadparwaiz1/cmp_luasnip",
+    },
+  },
+
+  -- Linting and formatting
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
   },
 
   -- Commentary plugin
@@ -39,41 +70,37 @@ require("lazy").setup({
   {
     "nvim-telescope/telescope.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
-    config = function()
-      local builtin = require("telescope.builtin")
-      vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-      vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
-      vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
-      vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
-    end,
   },
 
   -- File explorer
   {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("nvim-tree").setup{}
-      vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
-    end,
   },
 
   -- Status line
   {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    config = function()
-      require("lualine").setup{}
-    end,
   },
 
   -- Which-key for keybinding help
+  { "folke/which-key.nvim" },
+
+  -- Git integration
+  { "lewis6991/gitsigns.nvim" },
+
+  -- Terminal management
+  { "akinsho/toggleterm.nvim", version = "*" },
+
+  -- Markdown preview
   {
-    "folke/which-key.nvim",
-    config = function()
-      require("which-key").setup{}
-    end,
+    "iamcco/markdown-preview.nvim",
+    build = function() vim.fn["mkdp#util#install"]() end,
   },
+
+  -- Tailwind CSS support
+  { "williamboman/mason.nvim" },
 })
 
 -- General settings
@@ -83,7 +110,7 @@ vim.opt.smartcase = true
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.scrolloff = 5
-vim.opt.showmode = true
+vim.opt.showmode = false
 vim.opt.hidden = true
 vim.opt.backup = false
 vim.opt.writebackup = false
@@ -116,9 +143,9 @@ vim.cmd[[colorscheme dracula]]
 local keymap = vim.api.nvim_set_keymap
 local opts = { noremap = true, silent = true }
 
--- Move to beginning/end of line
-keymap("n", "H", "^", opts)
-keymap("n", "L", "$", opts)
+-- Buffer navigation
+keymap("n", "H", ":bprevious<CR>", opts)
+keymap("n", "L", ":bnext<CR>", opts)
 
 -- Move selected lines up/down
 keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
@@ -131,16 +158,8 @@ keymap("n", "U", "<C-R>", opts)
 keymap("n", "<leader>ve", ":e ~/.config/nvim/init.lua<CR>", opts)
 keymap("n", "<leader>vr", ":source ~/.config/nvim/init.lua<CR>", opts)
 
--- Better escape
-keymap("i", "jk", "<Esc>", opts)
-
 -- Replace entire buffer with system clipboard contents
 keymap("n", "<leader>sp", "ggVG\"+p", opts)
-
--- Tabs and buffers
-keymap("n", "<leader>]", ":bnext<CR>", opts)
-keymap("n", "<leader>[", ":bprevious<CR>", opts)
-keymap("n", "<leader>bd", ":bdelete<CR>", opts)
 
 -- Windows and splits
 keymap("n", "<leader>sv", ":vsplit<CR>", opts)
@@ -155,30 +174,173 @@ keymap("n", "<C-l>", "<C-w>l", opts)
 
 -- Save and quit
 keymap("n", "<leader>w", ":w<CR>", opts)
+keymap("n", "<leader>q", ":q<CR>", opts)
 keymap("n", "<leader>x", ":x<CR>", opts)
 
 -- Folding
-keymap("n", "<leader>fc", ":fold<CR>", opts)
 keymap("n", "<leader>fo", ":foldopen<CR>", opts)
-keymap("n", "<leader>fC", ":foldclose<CR>", opts)
-keymap("n", "<leader>fO", ":foldopen!<CR>", opts)
+keymap("n", "<leader>fc", ":foldclose<CR>", opts)
+keymap("n", "<leader>fu", ":foldopen!<CR>", opts)
 keymap("n", "<leader>fa", ":foldclose!<CR>", opts)
 
--- Marks
-keymap("n", "<leader>m", ":mark ", opts)
-keymap("n", "<leader>'", ":marks<CR>", opts)
-
--- Quick window switching
-keymap("n", "<leader>w", "<C-w>w", opts)
-
--- Commentary (using vim-commentary plugin)
+-- Commentary
 keymap("n", "<leader>/", ":Commentary<CR>", opts)
-
--- Terminal
-keymap("n", "<leader>t", ":terminal<CR>", opts)
 
 -- Clear search highlighting
 keymap("n", "<leader>h", ":noh<CR>", opts)
+
+-- Toggle options
+keymap("n", "<leader>z", ":ZenMode<CR>", opts)
+keymap("n", "<leader>\\", ":set wrap!<CR>", opts)
+
+-- Additional useful mappings
+keymap("n", "<leader>bd", ":bdelete<CR>", opts)
+keymap("n", "<leader>bn", ":enew<CR>", opts)
+keymap("n", "<leader>o", ":only<CR>", opts)
+
+-- Markdown preview
+keymap("n", "<leader>mp", ":MarkdownPreview<CR>", opts)
+
+-- Telescope mappings
+local builtin = require('telescope.builtin')
+vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+
+-- NvimTree
+vim.keymap.set('n', '<leader>e', ':NvimTreeToggle<CR>', {})
+
+-- Toggleterm
+vim.keymap.set('n', '<leader>t', ':ToggleTerm<CR>', {})
+
+-- LSP keybindings
+local on_attach = function(client, bufnr)
+  local bufopts = { noremap=true, silent=true, buffer=bufnr }
+  vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
+  vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+  vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, bufopts)
+  vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+  vim.keymap.set('n', '<leader>f', function() vim.lsp.buf.format { async = true } end, bufopts)
+end
+
+-- LSP setup
+local lspconfig = require('lspconfig')
+local mason = require('mason')
+local mason_lspconfig = require('mason-lspconfig')
+
+mason.setup()
+mason_lspconfig.setup({
+  ensure_installed = {
+    "pyright",
+    "tsserver",
+    "html",
+    "cssls",
+    "tailwindcss",
+    "dockerls",
+    "marksman",
+  },
+  automatic_installation = true,
+})
+
+-- Configure LSP servers
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
+
+local servers = { "pyright", "tsserver", "html", "cssls", "tailwindcss", "dockerls", "marksman" }
+for _, lsp in ipairs(servers) do
+  lspconfig[lsp].setup({
+    on_attach = on_attach,
+    capabilities = capabilities,
+  })
+end
+
+-- Autocompletion setup
+local cmp = require('cmp')
+local luasnip = require('luasnip')
+
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm {
+      behavior = cmp.ConfirmBehavior.Replace,
+      select = true,
+    },
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  }),
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    { name = 'buffer' },
+    { name = 'path' },
+  },
+})
+
+-- Linting and formatting setup
+local null_ls = require("null-ls")
+
+null_ls.setup({
+  sources = {
+    null_ls.builtins.formatting.black,
+    null_ls.builtins.formatting.prettier,
+    null_ls.builtins.diagnostics.eslint,
+    null_ls.builtins.diagnostics.flake8,
+  },
+})
+
+-- Additional plugin configurations
+require('gitsigns').setup()
+require('lualine').setup()
+require('nvim-tree').setup()
+require('telescope').setup()
+require('which-key').setup()
+require('toggleterm').setup()
+
+-- Markdown preview setup
+vim.g.mkdp_auto_start = 0
+vim.g.mkdp_auto_close = 1
+
+-- Autocommands
+vim.cmd[[
+  augroup custom_filetypes
+    autocmd!
+    autocmd FileType lua,yaml,json setlocal tabstop=2 shiftwidth=2
+    autocmd FileType python,javascript,typescript,html,css setlocal tabstop=4 shiftwidth=4
+    autocmd FileType markdown setlocal wrap linebreak
+  augroup END
+
+  augroup trim_whitespace
+    autocmd!
+    autocmd BufWritePre * :%s/\s\+$//e
+  augroup END
+]]
 
 -- Reminder comments
 -- zf - create fold
@@ -191,3 +353,6 @@ keymap("n", "<leader>h", ":noh<CR>", opts)
 -- q{a-z} - record macro
 -- @{a-z} - play macro
 -- @@ - replay last macro
+
+-- Note: Remember to run :Mason to install language servers
+-- and external tools: npm i -g prettier eslint, pip install black flake8
